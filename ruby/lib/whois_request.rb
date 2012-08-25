8208d88e211857718b30b83deea4133d00110f28
Mateusz Zawisza <mateusz@applicake.com>
2012-08-25 22:32:14 +0200

    uses proper whois server names

--- ruby/lib/whois_request.rb
+++ ruby/lib/whois_request.rb
@@ -1,11 +1,11 @@
 module Peertracker
   class WhoisRequest
     def get_as_info(ip_address)
-      `whois -m #{ip_address}`
+      `whois -h whois.radb.net #{ip_address}`
     end
 
     def get_country_info(ip_address)
-      `whois -l #{ip_address}`
+      `whois -h whois.lacnic.net #{ip_address}`
     end
   end
 end
